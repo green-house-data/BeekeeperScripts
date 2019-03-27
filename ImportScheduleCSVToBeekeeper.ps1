502f642e6d7862bf6952a8defc69fe983315467f
hannjohnf <47701125+hannjohnf@users.noreply.github.com>
2019-03-26 20:27:39 -0500

    Import Schedule and link to server group

--- ImportScheduleCSVToBeekeeper.ps1
+++ ImportScheduleCSVToBeekeeper.ps1
@@ -5,16 +5,17 @@
      [string]$CSVFileName
 )
 
-Function Create-Schedule {
+Function CreateSchedule {
     Param (
     [string][Parameter(Mandatory=$True)]$ScheduleName,
     [string][Parameter(Mandatory=$True)]$ScheduleJson
     )
     $requestUri = "$($Uri)"
     $requestUri = $requestUri += "Schedules"
-    $statusCode =$null
 
+    #Override Certificate validation behaviour so that self-signed certs, expired certs or non-trusted certs are not an issue
     [System.Net.ServicePointManager]::ServerCertificateValidationCallback = {$True}
+    #Create WebRequest
     $webRequest = [System.Net.WebRequest]::Create($requestURI)
     $webRequest.Accept = 'application/json'
     $webRequest.ContentType = 'application/json;odata.metadata=minimal'
@@ -42,6 +43,10 @@ Function Create-Schedule {
     If ($response.StatusCode -Match 'OK|Created')
     {
         Write-Host "Schedule ($ScheduleName) successfully created" -ForegroundColor "Green"
+    }
+    Else 
+    {
+        Write-Host "Schedule ($ScheduleName) was not successfully created" -ForegroundColor "Red"
     }
     $response.Close()
 
@@ -79,9 +84,10 @@ Function ClusterToSchedule {
     )
     $requestUri = "$($Uri)"
     $requestUri = $requestUri +=  "/Clusters(" + $ClusterId + ")/`$links/Schedules" 
-    $statusCode =$null
 
+    #Override Certificate validation behaviour so that self-signed certs, expired certs or non-trusted certs are not an issue
     [System.Net.ServicePointManager]::ServerCertificateValidationCallback = {$True}
+    #Create WebRequest
     $webRequest = [System.Net.WebRequest]::Create($requestURI)
     $webRequest.Accept = 'application/json'
     $webRequest.ContentType = 'application/json;odata.metadata=minimal'
@@ -104,10 +110,17 @@ Function ClusterToSchedule {
        $response = $_.exception.Response
     }
 
+
+    #Check to see if request was sucessfull
     If ($response.StatusCode -Match 'NoContent')
     {
         Write-Host "    Schedule ($ScheduleName) successfully linked with Cluster ($ClusterName)" -ForegroundColor "cyan"
     }
+    Else 
+    {
+        Write-Host "Schedule ($ScheduleName) was not successfully linked with Cluster ($ClusterName)" -ForegroundColor "Red"
+    }
+
     $response.Close()
 
 }
@@ -118,7 +131,7 @@ $URI = "http://localhost/BeekeeperApi/OPAS.svc/"
 $FileContents = Import-CSV -Path $CSVFileName
 Foreach($Line in $FileContents)
 {
-    $InSchedule = $Line| Select @{Name="Name";Expression={$_."Name"}}, @{Name="Clusters";Expression={$_."Clusters"}}, @{Name="DayNumber";Expression={$_."DayNumber"}}, @{Name="DayOffset";Expression={$_."DayOffset"}}, @{Name="MaxRuntime";Expression={$_."MaxRuntime"}}, @{Name="MonthIncrement";Expression={$_."MonthIncrement"}}, @{Name="OriginalDayNumber";Expression={$_."OriginalDayNumber"}}, @{Name="PatchDay";Expression={$_."PatchDay"}}, @{Name="Recurring";Expression={$_."Recurring"}}, @{Name="WeekNumber";Expression={$_."WeekNumber"}}, @{Name="Enabled";Expression={$_."Enabled"}}, @{Name="PatchDate";Expression={$_."PatchDate"}}
+    $InSchedule = $Line| Select-Object @{Name="Name";Expression={$_."Name"}}, @{Name="Clusters";Expression={$_."Clusters"}}, @{Name="DayNumber";Expression={$_."DayNumber"}}, @{Name="DayOffset";Expression={$_."DayOffset"}}, @{Name="MaxRuntime";Expression={$_."MaxRuntime"}}, @{Name="MonthIncrement";Expression={$_."MonthIncrement"}}, @{Name="OriginalDayNumber";Expression={$_."OriginalDayNumber"}}, @{Name="PatchDay";Expression={$_."PatchDay"}}, @{Name="Recurring";Expression={$_."Recurring"}}, @{Name="WeekNumber";Expression={$_."WeekNumber"}}, @{Name="Enabled";Expression={$_."Enabled"}}, @{Name="PatchDate";Expression={$_."PatchDate"}}
     $GroupList = $InSchedule.Clusters.split("|")
 
     $InSchedule.Clusters = $GroupList
@@ -144,7 +157,7 @@ Foreach($Line in $FileContents)
     $InSchedule.PatchDate = "{0:yyyy-MM-ddThh:mm:sszzz}" -f ([datetime]$InSchedule.PatchDate)
     $InSchedule.psobject.Properties.remove('Clusters')
     $InScheduleJson =  $InSchedule |ConvertTo-Json
-    Create-Schedule -ScheduleName $InSchedule.Name -ScheduleJson $InScheduleJson
+    CreateSchedule -ScheduleName $InSchedule.Name -ScheduleJson $InScheduleJson
     $ScheduleId = Get-New-Schedule-ID -ScheduleName $InSchedule.Name
     Foreach($Group in $GroupList)
     {
@@ -155,6 +168,10 @@ Foreach($Line in $FileContents)
            $ClusterToScheduleJson = $ClusterToSchedule | ConvertTo-Json
            ClusterToSchedule -ClusterId $CL_ID  -ClusterName $Group -ScheduleName $InSchedule.Name -ScheduleJson $ClusterToScheduleJson
         }
+        Else 
+        {
+           Write-Host "Cluster ($Group) does not exist, schedule cannot be linked" -ForegroundColor "Red"
+        }           
     }
 }
 
