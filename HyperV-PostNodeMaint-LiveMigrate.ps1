494daeae1cfdf2a1afb9b12b9329067b9fc7d9a8
hannjohnf <47701125+hannjohnf@users.noreply.github.com>
2019-09-27 14:52:46 -0500

    Add files via upload

---
+++ HyperV-PostNodeMaint-LiveMigrate.ps1
@@ -0,0 +1,62 @@
+﻿#==================================================================
+#     Post Maintenance Task to Live Migrate VMs after patching    =
+#==================================================================
+$ClusterName = "#!clustername!#"
+$ClusterNode = "#!nodename!#"
+Import-Module FailoverClusters
+Import-Module Hyper-V
+#======================================
+#     Get System Variables            =
+#======================================
+$URI_Headers = @{ 'Accept' = 'application/json' }
+$SettingsRequestURI = "http://localhost/BeekeeperApi/OPAS.svc/SystemVariables"
+$SettingResults = Invoke-RestMethod -Uri $SettingsRequestURI -UseDefaultCredentials -Headers $URI_Headers
+$MoveTimeout = 180
+Foreach($Variable in $SettingResults.value)
+{
+ If($Variable.Name -eq "Cluster Node Rebalance Resources Timeout")
+ {
+   $MoveTimeout = $Variable.Value 
+ }
+}
+#======================================
+#     Get VMs on Cluster              =
+#======================================
+$VMs = Get-ClusterGroup -Cluster $ClusterName | ? {$_.GroupType –eq 'VirtualMachine' -and $_.State -eq 'Online'}
+Foreach($VM in $VMs)
+{
+#======================================
+#     Get VM Preferred Owner          =
+#======================================
+  $PreferredOwners = Get-ClusterOwnerNode -Cluster $ClusterName -Group $VM.Name | Where-Object {$_.OwnerNodes -eq $ClusterNode}
+  If($PreferredOwners.count -gt 0)
+  {
+    If($VM.OwnerNode -ne $ClusterNode)
+    {
+#================================================
+#     Live Migrate VMs to Other Cluster Nodes   =
+#================================================
+      $WarningText = "Post-Maintenance Live Migrate: " + $VM.Name + " to " + $ClusterNode
+      Write-Warning $WarningText
+      Move-ClusterVirtualMachineRole $VM -Cluster $ClusterName -Node $ClusterNode -Wait $MoveTimeout -ErrorAction SilentlyContinue
+    }
+  }
+}
+
+
+$AfterVMs = Get-ClusterGroup -Cluster $ClusterName | ? {$_.GroupType –eq 'VirtualMachine' -and $_.State -eq 'Online' -and $_.OwnerNode -eq $ClusterNode} 
+If($AfterVMs.Count -gt 0)
+ {
+   Foreach($AfterVM IN $AfterVMs)
+   {
+    If($AfterVM.OwnerNode -ne $ClusterNode)
+     {
+     $ErrorText = "Post-Maintenance Live Migrate: " + $ClusterNode + " VM did not migrate: " + $AfterVM.Name
+     Write-Error $ErrorText
+     }
+   }
+ }
+ Else
+ {
+   $Error.Clear()
+ }
---
+++ HyperV-PreNodeMaint-LiveMigrate.ps1
@@ -0,0 +1,66 @@
+﻿#==================================================================
+#     PreNode Maintenance Task to Live Migrate VMs before patching=
+#==================================================================
+$ClusterName = "#!clustername!#"
+$ClusterNode = "#!nodename!#"
+
+Import-Module FailoverClusters
+Import-Module Hyper-V
+#======================================
+#     Get System Variables            =
+#======================================
+$URI_Headers = @{ 'Accept' = 'application/json' }
+$SettingsRequestURI = "http://localhost/BeekeeperApi/OPAS.svc/SystemVariables"
+$SettingResults = Invoke-RestMethod -Uri $SettingsRequestURI -UseDefaultCredentials -Headers $URI_Headers
+$MoveTimeout = 180
+Foreach($Variable in $SettingResults.value)
+{
+ If($Variable.Name -eq "Cluster Node Rebalance Resources Timeout")
+ {
+   $MoveTimeout = $Variable.Value 
+ }
+}
+#======================================
+#     Get Nodes in Cluster            =
+#======================================
+$OtherNodes = (get-clusternode -Cluster $ClusterName | Where-Object { $_.State -eq "Up" -and $_.Name -ne $ClusterNode })
+If($OtherNodes.count -eq 1)
+{
+   $TargetNode = $OtherNodes.Name
+}
+Else
+{
+   $Element = $OtherNodes.GetUpperBound(0)
+   $TargetNode = $OtherNodes[$Element].Name
+}
+#======================================
+#     Get VMs on Cluster              =
+#======================================
+$VMs = Get-ClusterGroup -Cluster $ClusterName | ? {$_.GroupType –eq 'VirtualMachine' -and $_.State -eq 'Online'} 
+Foreach($VM in $VMs)
+{
+   If($VM.OwnerNode -eq $ClusterNode)
+   {
+#================================================
+#     Live Migrate VMs to Other Cluster Nodes   =
+#================================================
+#================================================
+      $WarningText = "Pre-Maintenance Live Migrate: " + $VM.Name + " to " + $TargetNode
+      Write-Warning $WarningText
+      Move-ClusterVirtualMachineRole $VM -Cluster $ClusterName -Node $TargetNode -Wait $MoveTimeout -ErrorAction SilentlyContinue 
+     # $Error.Clear()
+   }
+}
+$AfterVMs = Get-ClusterGroup -Cluster $ClusterName | ? {$_.GroupType –eq 'VirtualMachine' -and $_.State -eq 'Online' -and $_.OwnerNode -eq $ClusterNode} 
+If($AfterVMs.Count -gt 0)
+ {
+   Foreach($AfterVM IN $AfterVMs)
+   {
+     $ErrorText = "Pre-Maintenance Live Migrate: " + $ClusterNode + " VM did not migrate: " + $AfterVM.Name
+     Write-Error $ErrorText
+   }
+ }
+Else
+ {
+   #$Error.Clear()
+ }
